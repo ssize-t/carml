c616ab1f6a9fcced1a0417ee8cf356d8d85badaa
George Kharchenko <hkharc2@uic.edu>
2019-05-13 20:24:45 -0500

    Add statement, expression mode to REPL

--- bin/cl.ml
+++ bin/cl.ml
@@ -7,6 +7,12 @@ let parse_expr buf =
     Ok (Parser.single_expression Lexer.micro lexbuf)
   with s -> Error (Exn.to_string s)
 
+let parse_stmt buf =
+  let lexbuf = Lexing.from_string buf in
+  try
+    Ok (Parser.single_statement Lexer.micro lexbuf)
+  with s -> Error (Exn.to_string s)
+
 let parse f =
   let lexbuf = Lexing.from_channel (In_channel.create f) in
   try
@@ -34,18 +40,30 @@ let repl_header = "
 Carml 0.0.1
 "
 
-let rec _repl st =
+type repl_mode = Expr | Stmt
+
+let rec _repl st (mode: repl_mode) =
   printf ">>> ";
   Out_channel.flush Out_channel.stdout;
   let line = In_channel.input_line In_channel.stdin in
   match line with
   | None -> ()
-  | Some "" -> _repl st
+  | Some "" -> _repl st mode
   | Some "exit" -> ()
+  | Some ":expr" -> printf "Switched to expression mode\n"; _repl st Expr
+  | Some ":stmt" -> printf "Switched to statement mode\n"; _repl st Stmt
   | Some l -> (
-    match parse_expr l with
-    | Error e -> printf "Parse error: %s" e; _repl st
-    | Ok s -> printf "%s" (Carml.Eval.show_value (Carml.Eval.eval_expr st s)); _repl st
+    match mode with
+    | Expr -> (
+      match parse_expr l with
+      | Error e -> printf "Parse error: %s" e; _repl st mode
+      | Ok s -> printf "%s\n" (Carml.Eval.show_value (Carml.Eval.eval_expr st s)); _repl st mode
+    )
+    | Stmt -> (
+      match parse_stmt l with
+      | Ok s -> _repl (Carml.Eval.eval_stmt st s) mode
+      | Error e -> printf "Parse error: %s" e; _repl st mode
+    )
   )
 
 let repl st =
@@ -69,7 +87,7 @@ let () =
   | "parse" -> parse (Array.nget Sys.argv 2)
   | "typecheck" -> typecheck (Array.nget Sys.argv 2)
   | "pretty" -> pretty (Array.nget Sys.argv 2)
-  | "repl"   -> repl Carml.Eval.empty_state
+  | "repl"   -> repl Carml.Eval.empty_state Expr
   | "-h" -> help ()
   | "--help" -> help ()
   | s -> printf "Unknown command: %s (-h/--help for help)\n" s
