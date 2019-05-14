498961e6fa2861a0824bc2853afe4c72c41554e8
George Kharchenko <hkharc2@uic.edu>
2019-05-14 17:55:48 -0500

    Allow multi-line statements in REPL; require ;;

--- bin/cl.ml
+++ bin/cl.ml
@@ -66,40 +66,48 @@ Type \":expr\" to enter expression mode
 You are in %s mode
 " (show_mode mode)
 
-let rec _repl st (mode: repl_mode) =
-  printf ">>> ";
+let rec _repl st (mode: repl_mode) (buf: string) =
+  printf "%s" (if buf = "" then ">>> " else "    ");
   Out_channel.flush Out_channel.stdout;
   let line = In_channel.input_line In_channel.stdin in
   match line with
   | None -> ()
-  | Some "" -> _repl st mode
-  | Some "exit" -> ()
-  | Some ":auto" -> printf "Switched to auto mode\n"; _repl st Auto
-  | Some ":expr" -> printf "Switched to expression mode\n"; _repl st Expr
-  | Some ":stmt" -> printf "Switched to statement mode\n"; _repl st Stmt
-  | Some ":help" -> printf "%s" (repl_help mode); _repl st mode
-  | Some l -> (
-    match mode with
-    | Expr -> (
-      match parse_expr l with
-      | Error e -> printf "Parse error: %s\n" e; _repl st mode
-      | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); _repl st mode
-    )
-    | Stmt -> (
-      match parse_stmt l with
-      | Ok s -> _repl (Carml.Eval.eval_stmt st s) mode
-      | Error e -> printf "Parse error: %s\n" e; _repl st mode
-    )
-    | Auto -> (
-      match parse_stmt l with
-      | Ok s -> _repl (Carml.Eval.eval_stmt st s) mode
-      | Error e -> (
-        match parse_expr l with
-        | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); _repl st mode
-        | Error e -> printf "Parse error: %s\n" e; _repl st mode
+  | Some "" -> _repl st mode buf
+  | Some "exit" when buf = "" -> ()
+  | Some ":auto" when buf = "" -> printf "Switched to auto mode\n"; _repl st Auto ""
+  | Some ":expr" when buf = "" -> printf "Switched to expression mode\n"; _repl st Expr ""
+  | Some ":stmt" when buf = "" -> printf "Switched to statement mode\n"; _repl st Stmt ""
+  | Some ":help" when buf = "" -> printf "%s" (repl_help mode); _repl st mode ""
+  | Some l when String.substr_index l ~pattern:";;" = Some (String.length l - 2) -> (
+    let l = buf ^ l in
+    let pos = String.substr_index_all l ~pattern:";;" ~may_overlap:false in
+    let lines, _ = List.fold pos ~f:(fun (acc, old_pos) new_pos -> (acc @ [(String.sub l ~pos:old_pos ~len:(new_pos-old_pos))], new_pos + 2)) ~init:(([], 0)) in
+    let st' = List.fold lines ~f:(fun st line -> (
+      match mode with
+      | Expr -> (
+        match parse_expr line with
+        | Error e -> printf "Parse error: %s\n" e; st
+        | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); st
       )
-    )
+      | Stmt -> (
+        match parse_stmt line with
+        | Ok s -> Carml.Eval.eval_stmt st s
+        | Error e -> printf "Parse error: %s\n" e; st
+      )
+      | Auto -> (
+        match parse_stmt line with
+        | Ok s -> Carml.Eval.eval_stmt st s
+        | Error e -> (
+          match parse_expr line with
+          | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); st
+          | Error e -> printf "Parse error: %s\n" e; st
+        )
+      )
+    )) ~init:st
+    in
+    _repl st' mode ""
   )
+  | Some l -> _repl st mode (buf ^ l)
 
 let repl st =
   printf "%s\n" repl_header;
@@ -123,7 +131,7 @@ let () =
   | "parse" -> parse (Array.nget Sys.argv 2)
   | "typecheck" -> typecheck (Array.nget Sys.argv 2)
   | "pretty" -> pretty (Array.nget Sys.argv 2)
-  | "repl"   -> repl Carml.Eval.empty_state Auto
+  | "repl"   -> repl Carml.Eval.empty_state Auto ""
   | "run"    -> run (Array.nget Sys.argv 2)
   | "-h" -> help ()
   | "--help" -> help ()
