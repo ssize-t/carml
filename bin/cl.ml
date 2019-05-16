d7f1067614b085c6bb10298dd14ed185f8b40d29
George Kharchenko <hkharc2@uic.edu>
2019-05-16 10:51:04 -0500

    Typecheck statements in REPL

--- bin/cl.ml
+++ bin/cl.ml
@@ -66,54 +66,64 @@ Type \":load <filename>\" to load a file into the current environment
 You are in %s mode
 " (show_mode mode)
 
-let rec _repl st (mode: repl_mode) (buf: string) =
+let rec _repl (st: Carml.Eval.state) (gamma: Carml.Typecheck.env) (mode: repl_mode) (buf: string) =
   printf "%s" (if buf = "" then ">>> " else "    ");
   Out_channel.flush Out_channel.stdout;
   let line = In_channel.input_line In_channel.stdin in
   match line with
   | None -> ()
-  | Some "" -> _repl st mode buf
+  | Some "" -> _repl st gamma mode buf
   | Some "exit" when buf = "" -> ()
-  | Some ":auto" when buf = "" -> printf "Switched to auto mode\n"; _repl st Auto ""
-  | Some ":expr" when buf = "" -> printf "Switched to expression mode\n"; _repl st Expr ""
-  | Some ":stmt" when buf = "" -> printf "Switched to statement mode\n"; _repl st Stmt ""
-  | Some ":help" when buf = "" -> printf "%s" (repl_help mode); _repl st mode ""
+  | Some ":auto" when buf = "" -> printf "Switched to auto mode\n"; _repl st gamma Auto ""
+  | Some ":expr" when buf = "" -> printf "Switched to expression mode\n"; _repl st gamma Expr ""
+  | Some ":stmt" when buf = "" -> printf "Switched to statement mode\n"; _repl st gamma Stmt ""
+  | Some ":help" when buf = "" -> printf "%s" (repl_help mode); _repl st gamma mode ""
   | Some l when String.substr_index l ~pattern:":load" = Some 0 -> (
     let filename = String.strip (String.substr_replace_first l ~pattern:":load" ~pos:0 ~with_:"") in
     match run filename with
-    | Some st' -> _repl st' mode ""
-    | None -> _repl st mode ""
+    | Some st' -> _repl st' gamma mode ""
+    | None -> _repl st gamma mode ""
   )
   | Some l when String.substr_index l ~pattern:";;" = Some (String.length l - 2) -> (
     let l = buf ^ l in
     let pos = String.substr_index_all l ~pattern:";;" ~may_overlap:false in
     let lines, _ = List.fold pos ~f:(fun (acc, old_pos) new_pos -> (acc @ [(String.sub l ~pos:old_pos ~len:(new_pos-old_pos))], new_pos + 2)) ~init:(([], 0)) in
-    let st' = List.fold lines ~f:(fun st line -> (
+    let (gamma', st') = List.fold lines ~f:(fun (gamma, st) line -> (
       match mode with
       | Expr -> (
         match parse_expr line with
-        | Error e -> printf "Parse error: %s\n" e; st
-        | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); st
+        | Error e -> printf "Parse error: %s\n" e; (gamma, st)
+        | Ok s -> (
+          printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); (gamma, st)
+        )
       )
       | Stmt -> (
         match parse_stmt line with
-        | Ok s -> Carml.Eval.eval_stmt st s
-        | Error e -> printf "Parse error: %s\n" e; st
+        | Ok s -> (
+          match Carml.Typecheck.typecheck_stmt gamma s with
+          | Ok gamma' -> (gamma', Carml.Eval.eval_stmt st s)
+          | Error (_, errs) -> Carml.Error.print_errs (List.map errs ~f:(fun (e, _) -> e)); (gamma, st)
+        )
+        | Error e -> printf "Parse error: %s\n" e; (gamma, st)
       )
       | Auto -> (
         match parse_stmt line with
-        | Ok s -> Carml.Eval.eval_stmt st s
+        | Ok s -> (
+          match Carml.Typecheck.typecheck_stmt gamma s with
+          | Ok gamma' -> (gamma', Carml.Eval.eval_stmt st s)
+          | Error (_, errs) -> Carml.Error.print_errs (List.map errs ~f:(fun (e, _) -> e)); (gamma, st)
+        )
         | Error e -> (
           match parse_expr line with
-          | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); st
-          | Error e -> printf "Parse error: %s\n" e; st
+          | Ok s -> printf "%s\n" (Carml.Eval.pretty_value (Carml.Eval.eval_expr st s)); (gamma, st)
+          | Error e -> printf "Parse error: %s\n" e; (gamma, st)
         )
       )
-    )) ~init:st
+    )) ~init:(gamma, st)
     in
-    _repl st' mode ""
+    _repl st' gamma' mode ""
   )
-  | Some l -> _repl st mode (buf ^ l)
+  | Some l -> _repl st gamma mode (buf ^ l)
 
 let repl st =
   printf "%s\n" repl_header;
@@ -137,7 +147,7 @@ let () =
   | "parse" -> parse (Array.nget Sys.argv 2)
   | "typecheck" -> typecheck (Array.nget Sys.argv 2)
   | "pretty" -> pretty (Array.nget Sys.argv 2)
-  | "repl"   -> repl Carml.Eval.empty_state Auto ""
+  | "repl"   -> repl Carml.Eval.empty_state Carml.Typecheck.empty_env Auto ""
   | "run"    -> run (Array.nget Sys.argv 2); ()
   | "-h" -> help ()
   | "--help" -> help ()
